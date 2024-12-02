60adfb2734bcfabff16293b392ddd65a2cf28f18
ld <zzw422339@163.com>
2024-12-03 00:27:14 +0800

    HttpIterator

---
+++ src/main/java/com/zzw/transfer/spring/boot/transfer/HttpIterator.java
@@ -0,0 +1,39 @@
+package com.zzw.transfer.spring.boot.transfer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class HttpIterator<E> implements Iterator<E>
+{
+
+    private Iterator<E> i;
+
+    @Override
+    public boolean hasNext()
+    {
+        if (i != null && i.hasNext())
+        {
+            return true;
+        }
+
+        List<E> data = getData();
+        if (data.isEmpty()) return false;
+
+        i = data.iterator();
+        return i.hasNext();
+    }
+
+    @Override
+    public E next()
+    {
+        return i.next();
+    }
+
+    private List<E> getData()
+    {
+        // HTTP 获取数据
+        // TODO 多线程 ?
+        return new ArrayList<>();
+    }
+}
